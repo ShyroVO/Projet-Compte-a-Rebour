ba3754cdc288949992e0bfff4e6cfd5bb1421d39
Laurie <73292431+ShyroVO@users.noreply.github.com>
2021-01-02 17:24:31 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let hours  = document.getElementById('hours');
+let minute = document.getElementById('minutes');
+let seconde = document.getElementById('seconde');
+let button = document.getElementById('envoi');
+
+button.addEventListener('click', function (){
+    console.log(typeof hours);
+
+})
