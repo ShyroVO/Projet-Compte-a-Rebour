91eb6b5598cc198c93cba4a802a03eb85f1ec723
Laurie <73292431+ShyroVO@users.noreply.github.com>
2021-01-03 17:45:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,47 +4,63 @@ let seconde = document.getElementById('seconde');
 let button = document.getElementById('envoi');
 let compteur = document.getElementById('compteurs');
 
-let i = 0;
-let hours = hour.value;
-let minutes = minute.value;
-let seconds = seconde.value;
+let Truc = function (heure, minute, seconde){
+    this.heure = heure;
+    this.minute = minute;
+    this.seconde = seconde;
+
+}
 
 function timerCreate () {
+    let i = 0;
+    let hours = hour.value;
+    let minutes = minute.value;
+    let seconds = seconde.value;
+
     let divCompteur = document.createElement('div');
+
+    divCompteur.innerHTML = hours + " h " + minutes + " min " + seconds + " s ";
     compteur.appendChild(divCompteur);
     divCompteur.id = "'" + "compteur" + i + "'";
-    console.log(hour.value + " h " + minute.value + " min " + seconde.value + " s ");
-    divCompteur.innerHTML = hours + " h " + minutes + " min " + seconds + " s ";
+
+    let countDown = function() {
+        let chrono = setTimeout(function (){
+            if (minutes === 0 && hours === 0 && seconds === 0){
+                divCompteur.innerHTML = hours + " h " + minutes + " min " + seconds + " s ";
+            }
+            else if (seconds > 0) {
+                divCompteur.innerHTML = hours + " h " + minutes + " min " + seconds + " s ";
+                seconds--;
+                countDown();
+            }
+            else if (seconds === 0){
+                minutes--;
+                seconds = 59;
+                divCompteur.innerHTML = hours + " h " + minutes + " min " + seconds + " s ";
+                countDown();
+                if (minutes === 0 && hours > 0){
+                    minutes = 59;
+                    seconds = 59;
+                    hours--;
+                    divCompteur.innerHTML = hours + " h " + minutes + " min " + seconds + " s ";
+                    countDown();
+                }
+                else if (minutes === 0 && hours === 0){
+                    seconds = 59;
+                    divCompteur.innerHTML = hours + " h " + minutes + " min " + seconds + " s ";
+                    countDown();
+                }
+            }
+
+        }, 1000);
+        document.getElementById('stop-button').addEventListener('click', function (){
+            clearTimeout(chrono);
+        })
+    };
 
     countDown();
     i++;
+
 }
-
-let countDown = function() {
-    let num = i;
-    let identifiant = document.getElementById("compteurs"+num);
-    seconds--;
-
-    let chrono = setTimeout(function (){
-
-        if (seconds > 0) {
-            identifiant.innerHTML = hours + " h " + minutes + " min " + seconds + " s ";
-            seconds--;
-            countDown();
-        }
-        else if (seconds === 0){
-            minutes--;
-            seconds = 59;
-
-            identifiant.innerHTML = hours + " h " + minutes + " min " + seconds + " s ";
-
-            countDown();
-        }
-    }, 1000);
-
-    document.getElementById('stop-button').addEventListener('click', function (){
-        clearTimeout(chrono);
-    })
-};
 
 button.addEventListener('click', timerCreate);
